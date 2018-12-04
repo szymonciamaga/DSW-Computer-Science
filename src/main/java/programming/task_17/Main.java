274c1e83549ecb2924deeb4d97b38bcdf06db9c8
Szymon Ciamaga <szymonciamaga@gmail.com>
2018-12-04 10:59:00 +0100

    import

--- src/main/java/programming/task_17/Main.java
+++ src/main/java/programming/task_17/Main.java
@@ -1,15 +1,14 @@
 package programming.task_17;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(
-                "1. Binary to decimal" +
-                "\n2. Decimal to binary" +
-                "\nOption: ");
-        int option = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("1. Binary to decimal" +
+//                        "\n2. Decimal to binary" +
+//                        "\n3. Decimal to Hexadecimal" +
+//                        "\nOption: ");
+        int option = 4;
         switch (option) {
             case 1:
                 int decResult = binaryToDecimal();
@@ -19,10 +18,107 @@ public class Main {
                 String binaryResult = decimalToBinary();
                 showResult(binaryResult);
                 break;
+            case 3:
+                decimalToHexadecimal();
+                break;
+            case 4:
+                hexadecimalToDecimal();
+                break;
                 default:
                     System.out.println("Incorrect option!");
                     break;
         }
+    }
+
+    private static void hexadecimalToDecimal() {
+        String value = "4A";
+        int base = 16;
+        int pow = 0;
+        int result = 0;
+        for (int i = value.length(); i > 0; i++) {
+            if (isInteger(value.charAt(i))) {
+                result += Math.pow(base, pow) * Integer.parseInt(String.valueOf(value.charAt(i)));
+            } else {
+                int thanEight = Integer.parseInt(moreThanEight(String.valueOf(value.charAt(i))));
+                System.out.println(Math.pow(base, pow) * thanEight);
+                result += Math.pow(base, pow) * thanEight;
+            }
+            pow++;
+        }
+        System.out.println("Result " + result);
+    }
+
+    private static boolean isInteger(char input) {
+        try {
+            Integer.parseInt(String.valueOf(input));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static void decimalToHexadecimal() {
+        int result = 1500000000;
+        int rest = 0;
+        String answer = "";
+        while (result >= 16) {
+            rest = result % 16;
+            result /= 16;
+            answer += rest >= 10 ? moreThanEight(String.valueOf(rest)) : String.valueOf(rest);
+            answer += result >= 10 ? moreThanEight(String.valueOf(result)) : String.valueOf(result);
+        }
+        showResult(reverseString(answer));
+    }
+
+    private static String reverseString(String result) {
+        String reverse = "";
+        for (int i = result.length() - 1; i >= 0; i--) {
+            reverse +=  result.charAt(i);
+        }
+        return reverse;
+    }
+
+    private static String moreThanEight(String number) {
+        String result = "";
+        switch (number) {
+            case "10":
+                result = "A";
+                break;
+            case "11":
+                result = "B";
+                break;
+            case "12":
+                result = "C";
+                break;
+            case "13":
+                result = "D";
+                break;
+            case "14":
+                result = "E";
+                break;
+            case "15":
+                result = "F";
+                break;
+            case "A":
+                result = "10";
+                break;
+            case "B":
+                result = "11";
+                break;
+            case "C":
+                result = "12";
+                break;
+            case "D":
+                result = "13";
+                break;
+            case "E":
+                result = "14";
+                break;
+            case "F":
+                result = "15";
+                break;
+        }
+        return result;
     }
 
     private static int binaryToDecimal() {
@@ -38,7 +134,7 @@ public class Main {
                     result += temp;
                 }
             }
-            temp = (temp * 2);
+            temp *= 2;
         }
         return result;
     }
